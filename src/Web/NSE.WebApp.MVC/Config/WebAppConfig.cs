43aecd613167094b54ac46ab62ec05c349db1c53
rafaacqv <rafaacqv@gmail.com>
2022-08-27 22:14:45 -0300

    MVC WebApp Configuration

---
+++ src/Web/NSE.WebApp.MVC/Config/WebAppConfig.cs
@@ -0,0 +1,26 @@
+﻿namespace NSE.WebApp.MVC.Config
+{
+    public static class WebAppConfig
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseIdentityConfiguration();
+
+        }
+    }
+}
