8898cedfe5c3aa50567a7403326c1b58af82b902
rafaacqv <rafaacqv@gmail.com>
2022-08-27 22:46:30 -0300

    Init MVC Login/Register Struct

--- src/Web/NSE.WebApp.MVC/Controllers/IdentityController.cs
+++ src/Web/NSE.WebApp.MVC/Controllers/IdentityController.cs
@@ -1,8 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentityController : Controller
     {
+        [HttpGet]
+        [Route("new-account")]
+        public IActionResult Register()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        [Route("new-account")]
+        public async Task<IActionResult> Register(UserRegister userRegister)
+        {
+            if(!ModelState.IsValid) return View(userRegister);
+
+            //API Registro
+            //Realizar login no App
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UserLogin userLogin)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        public async Task<IActionResult> Logout()
+        {
+
+        }
     }
 }
